7860bb41f8342335d55cf1b957ada0a9dd78e2b3
Marco <marco.amador.roman@gmail.com>
2023-02-13 22:44:03 -0600

    Contador

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-    var fechaNovios = new Date('July 10, 21 00:00:00');
+    var fechaNovios = new Date('2021,07,10');
     var fechaActual = new Date();
-    var diferencia = fechaActual.getTime() - fechaNovios.getTime() + (1000 * 60 * 60 *24);
+    var diferencia = fechaActual - fechaNovios - 3600000 - 1000;
     var diferenciaSegundos = diferencia /1000;
     var segundos = 0;
     var minutos = 0;
@@ -13,59 +13,71 @@ $(document).ready(function(){
             segundos++;
         }
         else{
-            segundos = 0;
+            segundos = 1;
             if (minutos != 60){
                 minutos++;
             }
             else{
-                minutos = 0;
+                minutos = 1;
                 if (horas != 24){
                     horas++;
                 }
                 else{
-                    horas = 0;
+                    horas = 1;
                     if (dias != 365){
                         dias++;
                     }
                     else{
-                        dias = 0;
+                        dias = 1;
                         años++;
                     }
                 }
             }
         }
     }
-    console.log(fechaNovios.getTime());
-    console.log(fechaActual.getTime());
-    console.log(diferencia);
-    console.log('Segundos: ', segundos);
-    console.log('Minutos: ', minutos);
-    console.log('Horas: ', horas);
-    console.log('Dias: ', dias);
-    console.log('Años: ', años);
-    //var tiempoVariable = setInterval(contador,1000);
+    $('#anhos').text(años);
+    $('#dias').text(dias);
+    $('#horas').text(horas);
+    $('#minutos').text(minutos);
+    $('#segundos').text(segundos);
+    intervalo = setInterval(contador,1000);
     function contador(){
+        console.log('K');
         if (segundos != 59){
             segundos++;
+            $('#segundos').text(segundos);
+            return;
         }
         else{
             segundos = 0;
+            $('#segundos').text(segundos);
             if (minutos != 59){
                 minutos++;
+                $('#minutos').text(minutos);
+                return;
             }
             else{
                 minutos = 0;
+                $('#minutos').text(minutos);
                 if (horas != 23){
                     horas++;
+                    $('#horas').text(horas);
+                    return;
                 }
                 else{
                     horas = 0;
+                    $('#horas').text(horas);
                     if (dias != 364){
                         dias++;
+                        $('#dias').text(dias);
+                        return;
                     }
                     else{
                         dias = 0;
+                        $('#dias').text(dias);
                         años++;
+                        $('#anhos').text(años);
+                        return;
                     }
                 }
             }
